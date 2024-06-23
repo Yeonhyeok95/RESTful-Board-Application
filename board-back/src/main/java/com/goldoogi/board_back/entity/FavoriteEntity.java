f22d3126e1e65ac4cca4c8631d1a073fc3d9edbe
Yeonhyeok Kim <connorkim996@gmail.com>
2024-06-23 14:45:40 +1000

    jpa entity, repository added

---
+++ board-back/src/main/java/com/goldoogi/board_back/entity/FavoriteEntity.java
@@ -0,0 +1,25 @@
+package com.goldoogi.board_back.entity;
+
+import com.goldoogi.board_back.entity.primaryKey.FavoritePk;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "favority")
+@Table(name = "favority")
+@IdClass(FavoritePk.class)
+public class FavoriteEntity {
+    
+    @Id
+    private String userEmail;
+    @Id 
+    private int boardNumber;
+}
---
+++ board-back/src/main/java/com/goldoogi/board_back/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.goldoogi.board_back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.goldoogi.board_back.entity.BoardEntity;
+
+@Repository
+public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
+}
